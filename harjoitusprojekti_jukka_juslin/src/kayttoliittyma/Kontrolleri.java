ca927d1187d2f60597b86767d435cb65116b7620
jusju <jusju@pao5009w03.haagahelia.amk>
2014-09-01 13:53:30 +0300

    eka versio servletista

---
+++ harjoitusprojekti_jukka_juslin/src/kayttoliittyma/Kontrolleri.java
@@ -0,0 +1,38 @@
+package kayttoliittyma;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Kontrolleri
+ */
+@WebServlet("/Kontrolleri")
+public class Kontrolleri extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Kontrolleri() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+	}
+
+}
